648037150d4ee10539308f380f578804ac5731fe
illiqa-lab <bendy20p@gmail.com>
2025-06-05 13:28:07 +0300

    двіжок Flask

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/index")
+@app.route("/")
+def index():
+	return "index"
+
+
+
+
+if __name__ == '__main__' :
+	app.run(host='127.0.0.1',port=5000,debug=True)
